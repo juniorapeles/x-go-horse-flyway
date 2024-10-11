92408d7689d16d534fbc18c4fa0c86a915529fe1
eli <elijunior.apeles2005@gmail.com>
2024-10-11 15:35:26 -0300

    first commit

---
+++ src/main/java/com/migrations/teste/x_go_horse/XGoHorseApplication.java
@@ -0,0 +1,13 @@
+package com.migrations.teste.x_go_horse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XGoHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XGoHorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migrations/teste/x_go_horse/XGoHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migrations.teste.x_go_horse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XGoHorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
