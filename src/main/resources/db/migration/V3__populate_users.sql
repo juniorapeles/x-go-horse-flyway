b9e4195bf4caebcb32ae031b3234763af0413800
eli <elijunior.apeles2005@gmail.com>
2024-10-11 16:39:39 -0300

    migrations com defeito

---
+++ src/main/resources/db/migration/V3__populate_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, age) VALUES
+('usuario1', 'senha1', 25),
+('usuario2', 'senha2', 30),
+('usuario3', 'senha3', 22),
+('usuario4', 'senha4', 28),
+('usuario5', 'senha5', 35);
---
+++ src/main/resources/db/migration/V4__add_email_to_users.sql
@@ -0,0 +1,1 @@
+UPDATE users SET email = CONCAT(username, '@example.com');
